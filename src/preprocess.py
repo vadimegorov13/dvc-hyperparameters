87b28d89d9eeb5903cfd9928fe0fc64f62e937da
Bex Tuychiev <bex@ibexprogramming.com>
2022-12-19 09:13:54 +0500

    Add and run 'preprocess' stage of a pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,9 +4,11 @@ from skimage.transform import resize
 from skimage.restoration import denoise_tv_chambolle
 from pathlib import Path
 from tqdm import tqdm
+from dvc.api import params_show
 
 DATA_DIR = Path("data")
 train_dir = DATA_DIR / "raw" / "train"
+test_dir = DATA_DIR / "raw" / "test"
 
 
 def save_image(image_array, image_path):
@@ -43,9 +45,11 @@ def denoise_image(image_path, weight):
 
 if __name__ == "__main__":
     image_paths = []
-    denoise_weight = 0.2
+    params = params_show()['preprocess']
+    denoise_weight = params["denoise_weight"]
 
-    for directory in train_dir.iterdir():
+    for directory in list(train_dir.iterdir()) + \
+                     list(test_dir.iterdir()):
         image_paths.extend(list(directory.glob("*.png")))
 
     Parallel(n_jobs=10, backend="multiprocessing")(
